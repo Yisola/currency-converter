40f78354160dda8b6ccc49dbc258f0aa9a0de0b7
Grace Akinremi <akinremiomoyisola@gmail.com>
2021-06-02 11:42:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -29,13 +29,20 @@ calculateBtn.addEventListener('click', function (e) {
 
     if (firstCurrency === "EUR") {
         xchangeRate.NAIRA = 504.71,
-        xchangeRate.USD = 1.12
+        xchangeRate.USD = 1.12,
+        xchangeRate.EUR = 1
     } else if (firstCurrency === "USD") {
         xchangeRate.NAIRA = 412.50,
-        xchangeRate.EUR = 0.82
+        xchangeRate.EUR = 0.82,
+        xchangeRate.USD = 1
     } else if (firstCurrency === "NAIRA") {
         xchangeRate.EUR = 0.0020,
-        xchangeRate.USD = 0.0024
+        xchangeRate.USD = 0.0024,
+        xchangeRate.NAIRA = 1
+    } else {
+        xchangeRate.EUR = 0,
+        xchangeRate.USD = 0,
+        xchangeRate.NAIRA = 0
     }
 
     let totalAmount = xchangeRate[secondCurrency] * userInput;
